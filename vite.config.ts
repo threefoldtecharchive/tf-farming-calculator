7803b9dff281e019c438015eaaae5405eee4cbab
MohamedElmdary <engm5081@gmail.com>
2023-03-12 11:41:40 +0200

    Update building configs

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: "./src/main.ts",
+      output: {
+        entryFileNames: "farmingcalculator.wc.js",
+      },
+    },
+  },
   plugins: [
     svelte({
       compilerOptions: {
